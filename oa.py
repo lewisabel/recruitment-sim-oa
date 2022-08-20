26faa7cbd701afd1f190b1c07385d6b319200083
Ilya Veksler <hi@ilyaveksler.com>
2022-08-20 11:56:13 -0700

    Create oa.py

---
+++ oa.py
@@ -0,0 +1,9 @@
+'''
+Returns the velocity of a ball after that has rolled down an incline.
+height: the height of the incline (meters)
+mass: the mass of the ball (kilograms)
+length: the length of the slope (meters)
+incline: the angle of the slope (degrees)
+'''
+def finalBallVelocity(self, height: int, length: int, incline: int, mass: int) -> int:
+    
