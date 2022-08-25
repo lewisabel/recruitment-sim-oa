57fe6215c0f74f8b6b18f1d57291833a40a989a3
Ilya Veksler <hi@ilyaveksler.com>
2022-08-24 17:18:16 -0700

    Update oa.py

--- oa.py
+++ oa.py
@@ -4,6 +4,7 @@ height: the height of the incline (meters)
 mass: the mass of the ball (kilograms)
 length: the length of the slope (meters)
 incline: the angle of the slope (degrees)
+kinetic friction coefficient: kinetic friction coefficient of the slope's surface (0.0 - 1.0)
 '''
-def finalBallVelocity(self, height: int, length: int, incline: int, mass: int) -> int:
+def finalBallVelocity(self, height: int, length: int, incline: int, mass: int, friction: float) -> int:
     
